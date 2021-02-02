1c3984cd74f023c63e55f8783d76e617ebdea7c4
Envoy-VC <65389981+Envoy-VC@users.noreply.github.com>
2021-02-02 11:06:42 +0000

    Deploying to gh-pages from  @ aff2370ca3b4cd949a962172dd29c4d38e261c8e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a9d3360197d358612ddf93cb7fdde733.js"
+  "/portfolio/precache-manifest.3c04fe3740745a5cd0e29e259ff7df14.js"
 );
 
 self.addEventListener('message', (event) => {
