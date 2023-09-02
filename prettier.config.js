e73ab16f8e81de2862922e7dd40f5aa426e08316
Vedant Chainani <vedantchainani1084@gmail.com>
2023-09-02 17:30:26 +0530

    project setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+
+module.exports = {
+	plugins: ['prettier-plugin-tailwindcss'],
+	trailingComma: 'es5',
+	tabWidth: 4,
+	semi: true,
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+};
