83cf7ddfb1d35a235b050abba30d393a8c7f1b86
Vedant Chainani <vedantchainani1084@gmail.com>
2025-08-30 07:17:38 +0530

    chore: initial commit

--- sections/index.ts
+++
@@ -1,8 +0,0 @@
-import Hero from './hero';
-import About from './about';
-import Blog from './blog';
-import Work from './work';
-import MoreProjects from './more-projects';
-import Contact from './contact';
-
-export { Hero, About, Blog, Work, MoreProjects, Contact };
